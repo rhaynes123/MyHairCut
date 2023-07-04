17a133b792a70f6fa272589883730686c2191756
Richard <rahayn@yahoo.com>
2023-07-03 21:49:41 -0400

    Correcting issues with day based notifications

--- My Haircut/My Haircut/Managers/ScheduleManager.swift
+++ My Haircut/My Haircut/Managers/ScheduleManager.swift
@@ -14,18 +14,23 @@ private let numberOfDaysForNextNotification = 7
 // MARK - Private Methods
 
 private func DetermineNextVisit(visit : Visit) -> DateComponents {
+    var dateComponent = DateComponents()
+    let currentCalendar = Calendar.current
+    
     let numOfDaysSetting = UserDefaults.standard.integer(forKey: "NumberOfDaysForNextNotificationSetting")
     let timeSetting = UserDefaults.standard.object(forKey: "timeUserSetting") as! Date
     
-    let currentCalendar = Calendar.current
+    
     let hourFromUserDefaults = currentCalendar.component(.hour, from: timeSetting)
     let minuteFromUserDefaults = currentCalendar.component(.minute, from: timeSetting)
     
     let dateOfVisit = visit.dateOfVisit!
-    var dateComponent = DateComponents()
+    let currentCalendarDate = currentCalendar.date(byAdding: dateComponent, to: dateOfVisit)
+    let futureDay = currentCalendar.component(.day, from: currentCalendarDate!) + numOfDaysSetting
     //dateComponent.day = 1
     dateComponent.hour = hourFromUserDefaults
     dateComponent.minute = minuteFromUserDefaults
+    dateComponent.day = futureDay
     return dateComponent
 }
 
